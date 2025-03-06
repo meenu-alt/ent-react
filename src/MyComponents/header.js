d0eea70559fe11ca78e12b007e5d2364990630da
Divyansh Vaish <dvisro13@gmail.com>
2025-03-06 15:56:03 +0530

    js

--- src/MyComponents/header.js
+++ src/MyComponents/header.js
@@ -2,6 +2,15 @@ import React from 'react'
 import logo from "../assets/images/logo.webp";
 import "../App.css";
 import "../App.js";
+import "../assets/css/style.css";
+import "../assets/css/bootstrap.min.css";
+import "../assets/css/animations.min.css";
+import "../assets/css/responsive.css";
+import "../assets/css/ionicons.min.css";
+import "../assets/css/jquery.mCustomScrollbar.min.css";
+import "../assets/css/magnific-popup.min.css";
+import "../assets/css/owl.carousel.min.css";
+import "../assets/css/progressbar.css";
 
 
 
