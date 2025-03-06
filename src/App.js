785291304e25acc0defb088e67746d5ad78ccdc8
meenu-alt <meenu.digitallybird@gmail.com>
2025-03-06 12:25:25 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import header from './MyComponents/header';
+
+function App() {
+  return (
+    <div className="App">
+      {8+9}
+    </div>
+  );
+}
+
+export default App;
