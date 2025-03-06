7548d443da4b2f07eaededb695486db37254fed2
Divyansh Vaish <dvisro13@gmail.com>
2025-03-06 15:10:36 +0530

    added

--- src/App.js
+++ src/App.js
@@ -3,6 +3,13 @@ import "./App.css";
 import Header from "./MyComponents/header"; // Ensure the correct path
 import "./assets/css/style.css";
 import "./assets/css/bootstrap.min.css";
+import "./assets/css/animations.min.css";
+import "./assets/css/responsive.css";
+import "./assets/css/ionicons.min.css";
+import "./assets/css/jquery.mCustomScrollbar.min.css";
+import "./assets/css/magnific-popup.min.css";
+import "./assets/css/owl.carousel.min.css";
+import "./assets/css/progressbar.css";
 
 function App() {
   return (
--- src/MyComponents/header.js
+++ src/MyComponents/header.js
@@ -1,5 +1,6 @@
 import React from 'react'
 
+
 export default function header() {
   return (
     <div ClassName="pq-bottom-header">
