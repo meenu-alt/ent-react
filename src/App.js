4050565751e708bb332b0d92d4af4b7be3bfea53
Divyansh Vaish <dvisro13@gmail.com>
2025-03-07 11:32:12 +0530

    push

--- src/App.js
+++ src/App.js
@@ -1,15 +1,13 @@
 import logo from "./logo.svg";
 import "./App.css";
-import Header from "./MyComponents/Header.js"//Ensure the correct path
-import Footer from "./MyComponents/Footer.js"
-
-
+import Header from "./MyComponents/Header.js"; //Ensure the correct path
+import Footer from "./MyComponents/Footer.js";
 
 function App() {
   return (
     <>
       <Header /> {/* Use the Header component */}
-      <Footer/>
+      <Footer />
     </>
   );
 }
