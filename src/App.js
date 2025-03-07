42b7fddb1627f43191a5b5438f9ca9609fccb79d
meenu-alt <meenu.digitallybird@gmail.com>
2025-03-07 10:17:14 +0530

    js

--- src/App.js
+++ src/App.js
@@ -1,16 +1,7 @@
 import logo from "./logo.svg";
 import "./App.css";
 import Header from "./MyComponents/header"; // Ensure the correct path
-import "./assets/css/style.css";
-import "./assets/css/bootstrap.min.css";
-import "./assets/css/animations.min.css";
-import "./assets/css/responsive.css";
-import "./assets/css/ionicons.min.css";
-import "./assets/css/jquery.mCustomScrollbar.min.css";
-import "./assets/css/magnific-popup.min.css";
-import "./assets/css/owl.carousel.min.css";
-import "./assets/css/progressbar.css";
-import './js/custom.js'
+
 
 
 function App() {
