c9ad09e76e6ff0d9e22091450705bd1ca631312c
meenu-alt <meenu.digitallybird@gmail.com>
2025-03-10 17:38:38 +0530

    dse

---
+++ src/MyComponents/services/ear/vertigo-clinic/Breadcrum.js
@@ -0,0 +1,33 @@
+import { FaHome } from "react-icons/fa";
+import { Outlet, Link } from "react-router-dom";
+
+const Breadcrumb = () => {
+  return (
+    <div
+      className=" bg-cover bdcrum">
+      <div className="container">
+        <div className="row align-items-center">
+          <div className="col-lg-12">
+            <nav aria-label="breadcrumb">
+              <div className="pq-breadcrumb-container mt-2">
+                <ol className="breadcrumb">
+                  <li className="breadcrumb-item">
+                    <Link to="/" className="mr-2">
+                      <FaHome className="mr-1" /> Home
+                    </Link>
+                  </li>
+                  <li className="breadcrumb-item active">Contact Us</li>
+                </ol>
+              </div>
+            </nav>
+          </div>
+          <div className="col-lg-4">
+            <div className="pq-breadcrumb-img text-right wow fadeInRight"></div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Breadcrumb;
--- src/MyComponents/services/ear/vertigo-clinic/index.js
+++ src/MyComponents/services/ear/vertigo-clinic/index.js
@@ -1,32 +1,12 @@
 import React from 'react'
-
+import Breadcrumb from './BreadCrum'
 
 export default function index() {
   return (
     <div>
-        <main>
-   <div className="pq-breadcrumb" >
-      <div className="container">
-         <div className="row align-items-center">
-            <div className="col-lg-12">
-               <nav aria-label="breadcrumb">
-                  <div className="pq-breadcrumb-title">
-                     <h2>Angioplasty services</h2> </div>
-                  <div className="pq-breadcrumb-container mt-2">
-                     <ol className="breadcrumb">
-                        <li className="breadcrumb-item"><a href="index.html"><i className="fas fa-home mr-2"></i>Home</a></li>
-                        <li className="breadcrumb-item active">Angioplasty Services</li>
-                     </ol>
-                  </div>
-               </nav>
-            </div>
-            <div className="col-lg-4">
-               <div className="pq-breadcrumb-img text-right wow fadeInRight"></div>
-            </div>
-         </div>
-      </div>
-   </div>
-        </main>
+      <main>
+         <Breadcrumb/>
+      </main>
     </div>
   )
 }
