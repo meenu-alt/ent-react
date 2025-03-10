9526c54d82c371029e28e36d455b2efc027f9936
meenu-alt <meenu.digitallybird@gmail.com>
2025-03-10 15:45:59 +0530

    index

--- src/App.js
+++ src/App.js
@@ -1,34 +1,32 @@
 import logo from "./logo.svg";
 import "./App.css";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Header from "./MyComponents/Header.js"; //Ensure the correct path
 import Footer from "./MyComponents/Footer.js";
-import AboutDoctor from "./MyComponents/home/AboutDoctor.js"
-import Banner from "./MyComponents/home/Banner.js";
-import WhatWeDo from "./MyComponents/home/WhatWeDo.js";
-import Testimonials from "./MyComponents/home/Testimonials.js";
-import PatientReviews from "./MyComponents/home/PatientReview.js";
-import FollowYt from "./MyComponents/home/FollowYt.js";
-import Faq from "./MyComponents/home/Faq.js";
-import Blogs from "./MyComponents/home/Blogs.js";
-import GetInTouch from "./MyComponents/home/GetInTouch.js";
+// import AboutDoctor from "./MyComponents/home/AboutDoctor.js"
+// import Banner from "./MyComponents/home/Banner.js";
+// import WhatWeDo from "./MyComponents/home/WhatWeDo.js";
+// import Testimonials from "./MyComponents/home/Testimonials.js";
+// import PatientReviews from "./MyComponents/home/PatientReview.js";
+// import FollowYt from "./MyComponents/home/FollowYt.js";
+// import Faq from "./MyComponents/home/Faq.js";
+// import Blogs from "./MyComponents/home/Blogs.js";
+// import GetInTouch from "./MyComponents/home/GetInTouch.js";
+import Home from "./MyComponents/home/Index.js";
+import About from "./MyComponents/about/index.js"
 
 
 
 function App() {
   return (
-    <>
-      <Header /> {/* Use the Header component */}
-      <Banner />
-      <AboutDoctor />
-      <WhatWeDo />
-      <Testimonials />
-      <PatientReviews />
-      <FollowYt />
-      <Faq />
-      <Blogs />
-      <GetInTouch />
-      <Footer />
-    </>
+    <Router>
+   <Header/>
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/about" element={<About />} />
+    </Routes>
+    <Footer />
+  </Router>
   );
 }
 
---
+++ src/MyComponents/about/index.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import page from './Page.js'
+
+export default function index() {
+  return (
+    <div>
+        <main>
+            <page/>
+            hiii
+        </main>
+    </div>
+  )
+}
