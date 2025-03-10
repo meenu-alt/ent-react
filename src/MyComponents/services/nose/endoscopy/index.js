d72f3cba525a3444cc404dc867ff97cd134854c6
Divyansh Vaish <dvisro13@gmail.com>
2025-03-10 17:06:50 +0530

    done

---
+++ src/MyComponents/services/nose/endoscopy/index.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+
+export default function index() {
+  return (
+    <div>
+        <main>
+            <page/>
+            ersdgfhvjbnm,gnhgvnoh9gyguasvbnmg nbiufhbdn v
+        </main>
+    </div>
+  )
+}
